c4763fb0b5d4b6cfd5de60d22eb9a1b42cc764a7
kekeli-the-light <kekelidompeh@gmail.com>
2023-08-04 10:41:01 +0000

    Regexes today

--- regex-learn.py
+++ regex-learn.py
@@ -5,3 +5,9 @@ for line in file:
     line = line.rstrip()
     if re.search('F..m:', line):
         print(line)
+
+#Extracting Data Using Regexes
+s = 'A message frome csev@umich.edu to cwen@iupui.edu about meeting @2PM'
+list = re.findall('\S+@\S+', s)
+print(list)
+
