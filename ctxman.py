d8f52dacc21d47901f9e06a5f6306191c331ff53
kekeli-the-light <kekelidompeh@gmail.com>
2023-08-20 14:04:41 +0000

    i dont know

--- ctxman.py
+++ ctxman.py
@@ -22,6 +22,7 @@ class OpenFile():
     def __enter__ (self):
         self.file = open(self.filename, self.mode)
         return self.file
+
     def __exit__ (self, exec_type, exc_val, traceback):
         self.file.close()
         
