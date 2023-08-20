8aeabc038c07a2e4fda729e641b3153515b1b1c2
kekeli-the-light <kekelidompeh@gmail.com>
2023-08-20 10:23:37 +0000

    more context managers

--- ctxman.py
+++ ctxman.py
@@ -8,8 +8,24 @@ def Open_file(filename, mode):
     finally:
         File.close()
         
-with Open_file('learn.txt', 'w') as File:
-    File.write('Mike is King')
+with Open_file('learn.txt', 'a') as File:
+    File.write('\nMike is King')
     
 print(File.closed)
     
+
+class OpenFile():
+    def __init__(self, filename, mode):
+        self.filename = filename
+        self.mode = mode
+    
+    def __enter__ (self):
+        self.file = open(self.filename, self.mode)
+        return self.file
+    def __exit__ (self, exec_type, exc_val, traceback):
+        self.file.close()
+        
+with OpenFile('learn.txt', 'a') as f:
+    f.write('\nThere was no need overwritting')
+
+print(f.closed)
