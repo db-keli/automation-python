b54d63ee6770024a8e0eaa4a95d6c652303ade6c
kekeli-the-light <kekelidompeh@gmail.com>
2023-08-24 08:28:15 +0000

    modular programming is damn fun

---
+++ modular/main.py
@@ -0,0 +1,18 @@
+from modir.modular import Student
+from modir.modular import Manager
+from modir.modular import Developer
+
+student = Student('James', 'Shakes', 6000)
+student2 = Student('Kofi', 'Bright', 700)
+
+print(student.email)
+student.set_raise_amount(300)
+print(student.apply_raise())
+
+manager = Manager('Mr', 'Sampson', 70_000, [student])
+manager.add_std(student2)
+manager.print_std()
+
+dev = Developer('Miss', 'Joyce', 7_302_002, 'Python')
+dev.set_raise_amount(78788)
+print(dev.apply_raise())
---
+++ modular/modir/modular.py
@@ -0,0 +1,91 @@
+class Student:
+    raise_amount = 104
+    num_of_emp = 0
+
+    def __init__(self, first, last, pay):
+        self.first = first
+        self.last = last
+        self.pay = pay
+
+        Student.num_of_emp += 1
+
+    @property
+    def email(self):
+        return 'mike{}@gmail.com'.format(self.last)
+
+    # @fullname.setter
+    # def fullname(self, name):
+    #     first, last = name.split
+
+    def __repr__(self):
+        return "Student('{}, {}, {}')".format(self.first, self.last, self.pay)
+
+    def __str__(self):
+        return "{} - {}".format(self.fullname(), self.email)
+
+    def fullname(self):
+        fullname = self.last+' '+self.first
+        return fullname
+
+    def apply_raise(self):
+        self.pay = int(self.pay * self.raise_amount)
+
+    @classmethod
+    def set_raise_amount(cls, amount):
+        cls.raise_amt = amount
+
+    @classmethod
+    def string_sep(cls, emp_str):
+        first, last, pay = emp_str.split('-')
+        return cls(first, last, pay)
+
+    @staticmethod
+    def is_workday(day):
+        if day.weekday() == 5 or day.weekday() == 6:
+            return False
+        return True
+
+
+class Developer(Student):
+    def __init__(self, first, last, pay, language):
+        super().__init__(first, last, pay)
+        self.language = language
+
+
+class Manager(Student):
+    def __init__(self, first, last, pay, students=None):
+        super().__init__(first, last, pay)
+        if students is None:
+            self.students = []
+        else:
+            self.students = students
+
+    def add_std(self, std):
+        if std not in self.students:
+            self.students.append(self)
+
+    def print_std(self):
+        for s in self.students:
+            print('-->', s)
+
+
+# std1 = Student.string_sep('Mike-Names-600')
+# dev = Developer('Mike', 'Billion', 7000, 'python')
+# man = Manager('sue', 'Smith', 7000, [dev])
+
+# man.print_std()
+# print(dev.email)
+# dev.apply_raise()
+# print(dev.pay)
+# print(dev.language)
+# Student.set_raise_amount(89)
+# print(std1.last)
+# print(std1.first)
+# print(std1.email)
+# print(std1.fullname())
+# print(std1.apply_raise())
+# print(std1.raise_amt)
+# print(Student.fullname(std1))
+# print(isinstance(Developer, Student))
+# print(std1.__repr__())
+# print(std1.__str__())
