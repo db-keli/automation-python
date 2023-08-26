104dc7bcbd24d45bf83790404897adfb34a25cb8
kekeli-the-light <kekelidompeh@gmail.com>
2023-08-26 14:54:00 +0000

    Aaron must use thisclear!

--- web-scraping/umat_sportal.py
+++ web-scraping/umat_sportal.py
@@ -3,10 +3,13 @@
 import webbrowser as wb
 import sys
 import pyperclip
+
+
 if len(sys.argv) > 1:
     spec = '/'.join(sys.argv[1:])
     address = f'https://portal.umat.edu.gh/{spec}'
 else:
     address = pyperclip.paste()
+
 
 wb.open(address)
