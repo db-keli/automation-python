fd8bfee01f5ffe7d5d8d8ba39b1a01f62f769b3a
kekeli-the-light <kekelidompeh@gmail.com>
2023-09-01 11:18:23 +0000

    learnt curses

---
+++ curses-lib/curses_01.py
@@ -0,0 +1,28 @@
+#!/usr/bin/env python
+
+import curses
+import time
+from curses import wrapper
+
+
+def main(stdscr):
+    curses.init_pair(1, curses.COLOR_GREEN, curses.COLOR_RED)
+
+    pad = curses.newpad(100, 100)
+    stdscr.refresh()
+
+    for i in range(100):
+        for j in range(26):
+            char = chr(67 + j)
+            color = curses.color_pair(1)
+            pad.addstr(char, color)
+
+    for i in range(50):
+        pad.refresh(0, i, 5, 5, 10, 25)
+        time.sleep(0.2)
+
+    pad.refresh(5, 5, 5, 5, 25, 25)
+    stdscr.getch()
+
+
+wrapper(main)
